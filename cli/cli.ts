3e749d7566356598d4dd8617ed3399d2530c8a29
gabosotob <69265038+gabosotob@users.noreply.github.com>
2024-06-14 00:37:59 -0600

    chore: update version

--- cli/cli.ts
+++ cli/cli.ts
@@ -8,7 +8,7 @@ const mainProgram = new Command();
 
 const mapCommand = (command: any) => mainProgram.addCommand(command);
 
-mainProgram.version('0.0.4').description('Zotto CLI tool: A tool to help you create Zotto elements.');
+mainProgram.version('0.0.5').description('Zotto CLI tool: A tool to help you create Zotto elements.');
 
 newElementCommands.forEach(mapCommand);
 mainProgram.addCommand(tsConfigCommand);
